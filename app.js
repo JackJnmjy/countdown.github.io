3eab5703d4a30b28e0b89183516c7ba6dc6c5ffd
Jack Janmejay <jackjanmejay@gmail.com>
2022-07-15 14:02:57 +0545

    countdown-timer

---
+++ app.js
@@ -0,0 +1,32 @@
+const countdown = () => {
+
+    //set the date we're counting down to
+    const countDate = new Date('July 16, 2022 12:38:00').getTime();
+
+    //get today's date and time
+    const now = new Date().getTime();
+
+    //find the distance between now and the count date
+    const gap = countDate - now;
+
+
+    //how time work?
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    //time calculation for days, hours, minutes and seconds
+    const textDay = Math.floor(gap / (day));
+    const textHour = Math.floor((gap % day) / hour);
+    const textMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute) / second); 
+   
+    
+    document.querySelector(".day").innerText = textDay;
+    document.querySelector(".hour").innerText = textHour;
+    document.querySelector(".minute").innerText = textMinute;
+    document.querySelector(".second").innerText = textSecond;
+};
+ 
+setInterval(countdown, 1000);
